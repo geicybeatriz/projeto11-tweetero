1e00910f524213a6a60efe3ff75f7f981206bf25
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-04-23 16:38:16 -0300

    feat: getting user data

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 import chalk from "chalk";
 import express, {json} from "express";
+import cors from "cors";
+
+// const userData = {
+// 	username: 'bobesponja', 
+// 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+// };
+
+// const tweetData = {
+// 	username: "bobesponja",
+// 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+//     tweet: "eu amo o hub",
+// };
+
+const users = [];
+const tweets = [];
 
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.post("/sign-up", (req, res) => {
+    const userData = req.body;
+    users.unshift(userData);
+
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const tweetRecebido = req.body;
+    tweets.unshift(tweetRecebido);
+
+    res.send("OK");
+});
+
+
 
 
 
