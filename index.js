26f303124842f28c876c3842cf0f7d7d50824284
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-04-23 17:37:18 -0300

    refactor: delete comments

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ import chalk from "chalk";
 import express, {json} from "express";
 import cors from "cors";
 
-// const userData = {
-// 	username: 'bobesponja', 
-// 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-// };
-
-// const tweetData = {
-// 	username: "bobesponja",
-// 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-//  tweet: "eu amo o hub",
-// };
-
 const users = [];
 const tweets = [];
 
